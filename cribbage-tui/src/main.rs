ab86cb625d3fc9389cf9de652e36c65742de1f35
Ron <ron@ronsbrain.com>
2025-09-04 11:16:25 -0400

    TUI WIP

--- cribbage-tui/src/main.rs
+++ cribbage-tui/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use ratatui::Frame;
+use ratatui::crossterm::event::{self, DisableMouseCapture, EnableMouseCapture, Event};
+use ratatui::crossterm::execute;
+use ratatui::crossterm::terminal::{
+    EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode,
+};
+use ratatui::prelude::{Backend, CrosstermBackend, Terminal};
+use std::io;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    run(&mut terminal)?;
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+    Ok(())
+}
+
+fn run<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<bool> {
+    loop {
+        terminal.draw(|f| ui(f))?;
+        if let Event::Key(_key) = event::read()? {
+            break;
+        }
+    }
+    Ok(true)
+}
+
+fn ui(frame: &mut Frame) {
+    frame.render_widget("Test", frame.area());
 }
